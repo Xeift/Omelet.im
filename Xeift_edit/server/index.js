bfe5c1bc0fb772d894fd7d70cc4083085b871566
xeift <xeift.c@gmail.com>
2023-05-15 09:28:46 +0800

    修改print內容

--- Xeift_edit/server/index.js
+++ Xeift_edit/server/index.js
@@ -5,28 +5,28 @@ const io = require('socket.io')(server);
 
 app.use(express.static('client'))
 
-app.get('/', (req, res) => { // 首頁
+app.get('/', (req, res) => { // 首頁的 html
     res.sendFile(__dirname + '/../client/index.html')
 })
 
 io.on('connection', (socket) => { // client 連上時執行的程式
-    console.log('connected!');
+    console.log('客戶端已連接');
 
     socket.on('greet', () => { // 2. 接收到 client 事件，回傳 greet 事件
-        socket.emit('greet', 'Hi! Client.');
+        socket.emit('greet', 'server端已接收到greet事件');
     });
 
     socket.on('disconnect', () => { // client 中斷連接時執行的程式
-        console.log('disconnected!');
+        console.log('客戶端已中斷連接');
     })
 
-    socket.on("send", (msg) => {
+    socket.on('send', (msg) => {
         // 如果 msg 內容鍵值小於 2 等於是訊息傳送不完全
         // 因此我們直接 return ，終止函式執行。
         if (Object.keys(msg).length < 2) return;
      
         // 廣播訊息到聊天室
-        io.emit("msg", msg);
+        io.emit('msg', msg);
     });
 })
 

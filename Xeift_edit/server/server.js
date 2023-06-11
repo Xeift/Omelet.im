048fb4550d61fa11eb195280201109519a948053
xeift <xeift.c@gmail.com>
2023-06-11 16:12:42 +0800

    remove online count

--- Xeift_edit/server/server.js
+++ Xeift_edit/server/server.js
@@ -5,17 +5,11 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const records = require('./record.js');  // 記錄歷史訊息
 
-
-let onlineCount = 0;
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/../client/index.html') // 首頁
 });
 
 io.on('connection', (socket) => { // 當成員連線
-    onlineCount++;
-    io.emit('online', onlineCount); // 發送人數給 client
-    socket.emit('maxRecord', records.getMax()); // 人數最大值
     socket.emit('chatRecord', records.get()); // 聊天記錄
     socket.on('send', (msg) => {
         if (Object.keys(msg).length < 2) return;
@@ -23,8 +17,7 @@ io.on('connection', (socket) => { // 當成員連線
     });
 
     socket.on('disconnect', () => { // 當成員離線
-        onlineCount = (onlineCount < 0) ? 0 : onlineCount-=1;
-        io.emit('online', onlineCount);
+        console.log('member disconnected');
     });
 });
 

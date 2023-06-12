f71e6405487165a7d5af3796a458dbd02f5a7bd7
xeift <xeift.c@gmail.com>
2023-06-12 14:33:57 +0800

    edit comment

--- Xeift_edit/mongotest/dddbbb.js
+++ Xeift_edit/mongotest/dddbbb.js
@@ -1,9 +1,7 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://xeift:M9Tuj4iOYjzM09Ns@omelet-cluster-1.svzxu5e.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(uri, { // mongo client
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -12,42 +10,27 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-
-
-
-
-    // 獲取一個數據庫對象，這裡我們選擇abc這個數據庫
-    const database = await client.db('sample_restaurants');
-
-    // 獲取一個集合對象，這裡我們選擇def這個集合
-    const collection = await database.collection('restaurants');
-
     try {
-        // 使用await關鍵字來等待findOne()方法的返回值，並賦值給doc變量
-        const doc = await collection.findOne({});
-        // 打印找到的文檔
-        console.log('Found the following document:');
-        console.log(doc);
-      } catch (err) {
-        // 捕獲並打印錯誤
-        console.error(err);
-      }
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!"); // 開發用，確認已成功連接
+
+        const database = await client.db('sample_restaurants'); // 連接名為 sample_restaurants 的 db
+        const collection = await database.collection('restaurants'); // 連接 sample_restaurants 中名為 restaurants 的 collection
+
+        try {
+            const doc = await collection.findOne({}); // 第一筆資料
+            console.log('Found the following document:');
+            console.log(doc);
+        }
+        catch (err) {
+            console.error(err);
+        }
 
 
-
-
-
-
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    } finally {
+        await client.close(); // 確保 client 中斷連接
+    }
 }
+
 run().catch(console.dir);

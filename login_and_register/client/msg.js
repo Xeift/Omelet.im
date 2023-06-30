84351201419164a840f20240452888e4d2123f44
Xeift <xeift.c@gmail.com>
2023-06-30 11:48:34 +0800

    edit comment

--- login_and_register/client/msg.js
+++ login_and_register/client/msg.js
@@ -1,16 +1,13 @@
-function getProtectRes() {
-    console.log('succ');
-    // 获取受保护的资源
+function getProtectRes() { // send protected requests
     fetch('/protected-resource', {
         method: 'GET',
         headers: {
-            'Authorization': localStorage.getItem('token')
+            'Authorization': localStorage.getItem('token') // get jwt
         }
     })
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-        document.getElementById('moring').innerHTML = data.decodedToken.id + data.decodedToken.username;
+        document.getElementById('moring').innerHTML = data.decodedToken.id + data.decodedToken.username; // display protected data
     })
     .catch(error => {
         console.error('请求发生错误:', error);

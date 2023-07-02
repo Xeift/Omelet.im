d668ba39323a209024ca4112013f873dc3b458e3
Xeift <xeift.c@gmail.com>
2023-07-02 16:38:01 +0800

    update login logic

---
+++ login_and_register/client/index.js
@@ -0,0 +1,36 @@
+let loginButton = document.getElementById('login-btn');
+loginButton.addEventListener('click', async function(event) {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    console.log(username, password);
+    // let data = {
+    //     username: username,
+    //     password: password
+    // };
+
+    // try {
+    //     let response = await fetch('/api/auth/login', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(data)
+    //     });
+
+    //     if (response.ok) {
+    //         let data = await response.json();
+
+    //         if (data.success) {
+    //             let token = data.token;
+    //             localStorage.setItem('token', token);
+    //             window.location.href = '/msg.html';
+    //         } else {
+    //             alert(data.message);
+    //         }
+    //     } else {
+    //         alert('Server error: ' + response.status);
+    //     }
+    // } catch (err) {
+    //     alert('Error: ' + err.message);
+    // }
+});

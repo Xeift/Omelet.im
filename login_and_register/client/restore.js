dcfa20d283c37ffab2a6d035c80993efad97bf6b
Xeift <xeift.c@gmail.com>
2023-07-07 17:11:56 +0800

    add retore auto fill in logic

--- login_and_register/client/restore.js
+++ login_and_register/client/restore.js
@@ -2,7 +2,7 @@ window.onload = function() {
     let tempEmailForRestore = localStorage.getItem('tempEmailForRestore');
     console.log(tempEmailForRestore);
     if ( !(tempEmailForRestore === null || tempEmailForRestore === undefined) ) {
-        alert('exsist');
+        document.getElementById('email').value = tempEmailForRestore;
     }
     localStorage.removeItem('tempEmailForRestore');
 }

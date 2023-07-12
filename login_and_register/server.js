be21fd44b9bf388d252ff31b865e93cfdf13e43d
Xeift <xeift.c@gmail.com>
2023-07-12 23:23:15 +0800

    minor fixes

--- login_and_register/server.js
+++ login_and_register/server.js
@@ -88,7 +88,7 @@ app.get('/protected-resource', auth.authenticateToken, (req, res) => { // protec
 });
 
 
-app.post("/reset", async (req, res) => {
+app.post('/reset', async (req, res) => {
     try {
         // Get the code and password from the request body
         const { code, password } = req.body;
@@ -103,13 +103,13 @@ app.post("/reset", async (req, res) => {
         // // Check if the user exists
         // if (!user) {
         //     // Send an error response
-        //     res.status(404).json({ message: "User not found" });
+        //     res.status(404).json({ message: 'User not found' });
         //     return;
         // }
         // // Update the user's password (use your own logic here)
         // await user.updatePassword(password);
         // // Send a success response
-        res.status(200).json({ message: "Password reset successfully" });
+        res.status(200).json({ message: 'Password reset successfully' });
     }
     catch (error) {
         // Send an error response

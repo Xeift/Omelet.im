6f33b5f3068e6974b7c93368f7eb00755d89ee7a
Xeift <xeift.c@gmail.com>
2023-07-20 14:03:50 +0800

    remove unuse import

--- login_and_register/config/mongodb.js
+++ login_and_register/config/mongodb.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const MONGO_URI = process.env.MONGO_URI;
-const utils = require('../utils/userUtils.js')
 
 
 mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}); // connect to mongodb

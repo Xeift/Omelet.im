3468b2656eb07f4644a5387313e59b5fecb11551
Xeift <xeift.c@gmail.com>
2023-07-21 18:52:22 +0800

    add code in home-register.js

--- login_and_register/client/home-register.js
+++ login_and_register/client/home-register.js
@@ -1,11 +1,35 @@
 let registerButton = document.getElementById('register-btn');
 registerButton.addEventListener('click', async function(event) {
     let hintMsg = document.getElementById('hint-msg');
-    let username = document.getElementById('username').value;
+    let email = document.getElementById('username').value;
     let password = document.getElementById('password').value;
+    let data = {
+        email: email
+    };
 
+    try {
+        let response = await fetch('/api/auth/verify-email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        let responseStatus = response.status
+        let responseData = await response.json();
 
-    
-
+        if (responseStatus === 200) {
+            hintMsg.innerHTML = 'email 已成功寄出';
+        }
+        else if (responseStatus === 401) {
+            hintMsg.innerHTML = 'email 不存在';
+        }
+        else if (responseStatus === 500) {
+            hintMsg.innerHTML = responseData.message;
+        }
+    }
+    catch (err) {
+        hintMsg.innerHTML = `前端發生例外錯誤： ${err.message}`;
+    }
 
 })

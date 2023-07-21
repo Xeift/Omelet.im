fd73401fd025e977d972486c9df23d95a9fc9b5d
Xeift <xeift.c@gmail.com>
2023-07-21 10:21:01 +0800

    add hintbox

--- login_and_register/client/home.js
+++ login_and_register/client/home.js
@@ -1,8 +1,8 @@
 let loginButton = document.getElementById('login-btn');
+let hintMsg = document.getElementById('hint-msg');
 loginButton.addEventListener('click', async function(event) {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
-    let hintMsg = document.getElementById('hint-msg');
 
     
     try {
@@ -46,16 +46,19 @@ let registerButton = document.getElementById('register-btn');
 registerButton.addEventListener('click', async function(event) {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
-    let hintMsg = document.getElementById('hint-msg');
+    // let hintMsg = document.getElementById('hint-msg');
 
     if (isEmailFormatValid(username)) { // username = email, username2 = username
         document.getElementById('usernameLabel').innerText = 'email';
         addThirdInputBox('使用者名稱');
-
+        hintMsg.innerText = '請輸入使用者名稱';
+        console.log('emi');
     }
     else { // username = username, username2 = email
         document.getElementById('usernameLabel').innerText = '使用者名稱';
         addThirdInputBox('email');
+        hintMsg.innerText = '請輸入email';
+        console.log('usr');
     }
 
     function addThirdInputBox(content) {
@@ -71,45 +74,45 @@ registerButton.addEventListener('click', async function(event) {
         }
     }
 
-    try {
-        if (isEmailFormatValid(username)) {
-            let response = await fetch('/api/auth/register', { // TODO: backend check
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    username: username,
-                    password: password
-                })
-            });
-            let responseStatus = response.status
-            let responseData = await response.json();
+    // try {
+    //     if (isEmailFormatValid(username)) {
+    //         let response = await fetch('/api/auth/register', { // TODO: backend check
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             },
+    //             body: JSON.stringify({
+    //                 username: username,
+    //                 password: password
+    //             })
+    //         });
+    //         let responseStatus = response.status
+    //         let responseData = await response.json();
     
-            if (responseStatus === 200) {
-                let token = responseData.token;
-                localStorage.setItem('token', token);
-                hintMsg.innerHTML = '登入成功';
-                window.location.href = '/msg';
-            }
-            else if (responseStatus === 401) {
-                let isUserExsists = responseData.data.isUserExsists;
-                console.log(isUserExsists);
-                hintMsg.innerHTML = isUserExsists ? '帳號或密碼錯誤，可重置密碼' : '帳號不存在，可註冊';
-            }
-            else if (responseStatus === 500) {
-                hintMsg.innerHTML = responseData.message;
-            }
-        }
-        else {
-            hintMsg.innerHTML = '';
-        }
+    //         if (responseStatus === 200) {
+    //             let token = responseData.token;
+    //             localStorage.setItem('token', token);
+    //             hintMsg.innerHTML = '登入成功';
+    //             window.location.href = '/msg';
+    //         }
+    //         else if (responseStatus === 401) {
+    //             let isUserExsists = responseData.data.isUserExsists;
+    //             console.log(isUserExsists);
+    //             hintMsg.innerHTML = isUserExsists ? '帳號或密碼錯誤，可重置密碼' : '帳號不存在，可註冊';
+    //         }
+    //         else if (responseStatus === 500) {
+    //             hintMsg.innerHTML = responseData.message;
+    //         }
+    //     }
+    //     else {
+    //         hintMsg.innerHTML = '';
+    //     }
 
 
-    }
-    catch (err) {
-        hintMsg.innerHTML = `前端發生例外錯誤： ${err.message}`;
-    }
+    // }
+    // catch (err) {
+    //     hintMsg.innerHTML = `前端發生例外錯誤： ${err.message}`;
+    // }
 })
 
 

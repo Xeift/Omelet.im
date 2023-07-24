7cd8de5e188f605dd06ba2ca582668209edcb95a
Xeift <xeift.c@gmail.com>
2023-07-24 17:17:11 +0800

    refactor home files in client folder

--- login_and_register/api/get/homePage.js
+++ login_and_register/api/get/homePage.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 
 module.exports = (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/home.html'));
+    res.sendFile(path.join(__dirname, '../../client/home/home.html'));
 };

a857b661d4b3ed696a1840a5ee59ad0d6da473dd
Xeift <xeift.c@gmail.com>
2023-08-07 23:44:30 +0800

    suppress unused vars in msg.js

--- login_and_register/client/login/msg.js
+++ login_and_register/client/login/msg.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 function getProtectRes() { // send protected requests
     fetch('/protected-resource', {
         method: 'GET',
@@ -5,12 +6,12 @@ function getProtectRes() { // send protected requests
             'Authorization': localStorage.getItem('token') // get jwt
         }
     })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('moring').innerHTML = data.decodedToken.id +'<br>'+ data.decodedToken.username; // display protected data
-    })
-    .catch(error => {
-        console.error('錯誤', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('moring').innerHTML = data.decodedToken.id +'<br>'+ data.decodedToken.username; // display protected data
+        })
+        .catch(error => {
+            console.error('錯誤', error);
+        });
 }
 

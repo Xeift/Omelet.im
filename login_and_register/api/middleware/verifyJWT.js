423c79be092e33def73c25b741e2423e54a1f907
Xeift <xeift.c@gmail.com>
2023-08-14 23:04:57 +0800

    remove unuse code

--- login_and_register/api/middleware/verifyJWT.js
+++ login_and_register/api/middleware/verifyJWT.js
@@ -3,7 +3,6 @@ const jwt = require('../../utils/jwt.js');
 
 module.exports = async(req, res, next) => { // jwt verify middleware TODO: verify jwt middle ware
     const token = req.headers['authorization']; // get jwt in header
-    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImExMTA1MTAzMjJAZ20udXNjLmVkdS50dyIsImlhdCI6MTY5MTY4MjgyMiwiZXhwIjoxNjkxNjgzNDIyfQ.z-piVruMC24roeP3d4DPqDBd3hOPgnUy4fpY0XQK1UQ';
     if (!token) {
         return res.status(401).json({ success: false, message: '未提供JWT' });
     }

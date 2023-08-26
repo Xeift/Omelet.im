b83c4bf9ee0cf3ae8ecfc5e75a090adf263a8baf
Xeift <xeift.c@gmail.com>
2023-08-26 15:39:32 +0800

    edit frontend home

--- frontend/server.js
+++ frontend/server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/client')); // set express static folder path
+app.use(express.static(__dirname + '/view')); // set express static folder path
 app.use(bodyParser.json()); // deal with json requests
 
 
-app.get('/', require('./api/get/homePage.js'));
-app.get('/register', require('./api/get/registerPage.js'));
+app.get('/', require('./route/homePage.js'));
 
 
 app.listen(3001, () => { // start server at port 3001
---
+++ frontend/view/home/home-forgot-password.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+let forgotPasswordButton = document.getElementById('forgot-password-btn');
+forgotPasswordButton.addEventListener('click', async function(event) {
+    let username = document.getElementById('username').value;
+    if (username !== null && username !== undefined && username !== '' && isEmailFormatValid(username)) {
+        localStorage.setItem('tempEmailForRestore', username);
+    }
+    window.location.href = '/forgot-password';
+});

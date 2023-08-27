05cde7b03f54b774a963f852eb7b0bcabd6dd999
Xeift <xeift.c@gmail.com>
2023-08-27 11:59:38 +0800

    add comment

--- backend/utils/mongodb.js
+++ backend/utils/mongodb.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: 'config/.env' });
 const MONGO_URI = process.env.MONGO_URI;
 
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); // 連接至 Mongodb
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); // connect to mongodb
-
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({ // 建立 UserSchema
     username: { type: String, unique: true },
     email: { type: String, unique: true },
     password: String,
     reset_temp_code: { type: String, unique: true },
 });
-const UserModel = mongoose.model('User', UserSchema);
+
+const UserModel = mongoose.model('User', UserSchema); // 建立 UserModel
--- frontend/server.js
+++ frontend/server.js
@@ -5,9 +5,7 @@ const bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/view')); // set express static folder path
 app.use(bodyParser.json()); // deal with json requests
 
-
 app.get('/', require('./route/homePage.js'));
-
 
 app.listen(3001, () => { // start server at port 3001
     console.log('伺服器已啟動\nhttp://localhost:3001');

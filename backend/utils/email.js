a4c115630d53409b4c47bc0ba05e29c324282204
Xeift <xeift.c@gmail.com>
2023-09-20 20:53:45 +0800

    fix email.js

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -4,55 +4,41 @@ const GMAIL_APP_PASSWORD = process.env.GMAIL_APP_PASSWORD;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 async function sendResetPasswordMail(email, code) {
-    try {
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'elpma.res@gmail.com',
-                pass: GMAIL_APP_PASSWORD
-            }
-        });
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'elpma.res@gmail.com',
+            pass: GMAIL_APP_PASSWORD
+        }
+    });
 
-        const mailOptions = {
-            from: 'elpma.res@gmail.com',
-            to: email,
-            subject: 'Omelet - 重置密碼申請',
-            html: `<h1>Omelet - 重置密碼申請</h1><p>我們收到您重置密碼的申請。<br>若您並未請求重置密碼，請忽略此郵件。<br>若您要重設密碼，請點擊下方藍字並按照網頁中的指示操作。</p><a href="${FRONTEND_URL}update-password?code=${code}">按我重置密碼<a>`
-        };
+    const mailOptions = {
+        from: 'elpma.res@gmail.com',
+        to: email,
+        subject: 'Omelet - 重置密碼申請',
+        html: `<h1>Omelet - 重置密碼申請</h1><p>我們收到您重置密碼的申請。<br>若您並未請求重置密碼，請忽略此郵件。<br>若您要重設密碼，請點擊下方藍字並按照網頁中的指示操作。</p><a href="${FRONTEND_URL}update-password?code=${code}">按我重置密碼<a>`
+    };
 
-        transporter.sendMail(mailOptions);
-    }
-    catch (err) {
-        console.log(err);
-        return false;
-    }
-    return true;
+    transporter.sendMail(mailOptions);
 }
 
 async function sendRegisterMail(email, code) {
-    try {
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'elpma.res@gmail.com',
-                pass: GMAIL_APP_PASSWORD
-            }
-        });
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'elpma.res@gmail.com',
+            pass: GMAIL_APP_PASSWORD
+        }
+    });
 
-        const mailOptions = {
-            from: 'elpma.res@gmail.com',
-            to: email,
-            subject: 'Omelet - 帳號註冊',
-            html: `<h1>Omelet - 帳號註冊</h1><p>我們收到您帳號註冊的申請。<br>若您並未請求註冊帳號，請忽略此郵件。<br>若您要註冊帳號，請點擊下方藍字並按照網頁中的指示操作。</p><a href="${FRONTEND_URL}register?code=${code}">按我註冊帳號<a>`
-        };
+    const mailOptions = {
+        from: 'elpma.res@gmail.com',
+        to: email,
+        subject: 'Omelet - 帳號註冊',
+        html: `<h1>Omelet - 帳號註冊</h1><p>我們收到您帳號註冊的申請。<br>若您並未請求註冊帳號，請忽略此郵件。<br>若您要註冊帳號，請點擊下方藍字並按照網頁中的指示操作。</p><a href="${FRONTEND_URL}register?code=${code}">按我註冊帳號<a>`
+    };
 
-        transporter.sendMail(mailOptions);
-    }
-    catch (err) {
-        console.log(err);
-        return false;
-    }
-    return true;
+    transporter.sendMail(mailOptions);
 }
 
 module.exports = { sendResetPasswordMail, sendRegisterMail };

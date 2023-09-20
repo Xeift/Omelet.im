aa2de42086d236a84ca01072608d77f6d98e1706
Xeift <xeift.c@gmail.com>
2023-09-20 19:30:58 +0800

    refactor messageController.js

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -2,26 +2,14 @@ const RoomModel = require('../model/roomModel');
 const snowflakeId = require('../utils/snowflakeId');
 
 async function createNewRoom(name, type) {
-    try {
-        let rid = snowflakeId.generateId();
-        let updatedRoom = await RoomModel.create({
-            rid: rid,
-            name: name,
-            type: type,
-            timestamp: snowflakeId.extractTimeStampFromId(rid),
-            members: []
-        });
-
-        if (updatedRoom) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    catch (err) {
-        return `後端發生例外錯誤： ${err.message}`;
-    }
+    let rid = snowflakeId.generateId();
+    let updatedRoom = await RoomModel.create({
+        rid: rid,
+        name: name,
+        type: type,
+        timestamp: snowflakeId.extractTimeStampFromId(rid),
+        members: []
+    });
 }
 
 module.exports = { createNewRoom };

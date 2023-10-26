1bdf088d1b14c8cafc0861c46b080b9158e13367
Xeift <xeift.c@gmail.com>
2023-10-26 18:12:17 +0800

    edit timestamp type from Number to String

--- backend/model/msgModel.js
+++ backend/model/msgModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('./mongoose');
 
 const UnreadMsgSchema = new mongoose.Schema({
-    timestamp: { type: Number, required: true },
+    timestamp: { type: String, required: true },
     type: { type: String, enum: ['text', 'image'], required: true },
     receiver: { type: String, required: true },
     sender: { type: String, required: true },

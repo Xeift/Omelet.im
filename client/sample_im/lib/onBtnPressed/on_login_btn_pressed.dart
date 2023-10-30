e2c9a461e7c9ecdd82c4bfabf0cdb00c731438aa
Xeift <xeift.c@gmail.com>
2023-10-30 14:08:05 +0800

    remove debug print

--- client/sample_im/lib/onBtnPressed/on_login_btn_pressed.dart
+++ client/sample_im/lib/onBtnPressed/on_login_btn_pressed.dart
@@ -1,13 +1,9 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Future<void> onLoginBtnPressed(String apiBaseUrl, String username,
     String password, Function updateHintMsg) async {
-  print('test');
-
   final res = await http.post(
     Uri.parse('$apiBaseUrl/api/v1/login'),
     headers: <String, String>{
@@ -19,8 +15,6 @@ Future<void> onLoginBtnPressed(String apiBaseUrl, String username,
   const storage = FlutterSecureStorage();
 
   final resBody = jsonDecode(res.body);
-
-  print(resBody);
 
   await storage.write(key: 'token', value: resBody['token']);
   updateHintMsg(

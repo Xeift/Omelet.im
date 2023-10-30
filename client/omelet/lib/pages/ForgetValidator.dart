7ca5a4d73bb6cb07520c4b2c94a01c262eac4b72
Maple <maple@MapledeMacBook-Pro.local>
2023-10-30 15:07:39 +0800

    forget_page_add

---
+++ client/omelet/lib/pages/ForgetValidator.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ValidationPage extends StatefulWidget{
+  const ValidationPage({Key? key}):super(key: key);
+  @override
+  _ValidationPageState createState() => _ValidationPageState();
+}
+
+class _ValidationPageState extends State<ValidationPage>{
+  late String _ValidationNum = '';
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+       appBar: AppBar(
+        leading: IconButton(
+          icon:const Icon(Icons.arrow_back_ios),
+          onPressed: () => Navigator.pop(context),
+            
+          ),
+      ),
+    );
+  }
+
+}

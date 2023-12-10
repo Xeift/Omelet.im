9c53c4e1ef228e0e9e36149df4f26b84a227cfeb
Xeift <xeift.c@gmail.com>
2023-12-10 13:17:13 +0800

    add safe_pre_key_store.dart

---
+++ client/sample_im/lib/sg_store/safe_pre_key_store.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
+
+class SafePreKeyStore implements PreKeyStore {
+  final FlutterSecureStorage storage;
+
+  SafePreKeyStore(this.storage);
+
+  static const String preKey = 'preKey';
+
+  @override
+  Future<bool> containsPreKey(int preKeyId) async {
+    final preKeys = jsonDecode((await storage.read(key: preKey)).toString());
+    if (preKeys == null) {
+      throw InvalidKeyIdException('no prekey found');
+    }
+
+    final singlePreKey = preKeys[preKeyId.toString()];
+
+    return singlePreKey != null;
+  }
+
+  @override
+  Future<PreKeyRecord> loadPreKey(int preKeyId) async {
+    final preKeys = jsonDecode((await storage.read(key: preKey)).toString());
+    if (preKeys == null) {
+      throw InvalidKeyIdException('no prekey found');
+    }
+
+    final singlePreKey = preKeys[preKeyId.toString()];
+    if (singlePreKey == null) {
+      throw InvalidKeyIdException('No such prekey record: $preKeyId');
+    }
+
+    return PreKeyRecord.fromBuffer(
+        Uint8List.fromList(jsonDecode(singlePreKey).cast<int>().toList()));
+  }
+
+  @override
+  Future<void> removePreKey(int preKeyId) async {
+    var preKeys = jsonDecode((await storage.read(key: preKey)).toString());
+    if (preKeys == null) {
+      throw InvalidKeyIdException('no prekey found');
+    }
+
+    preKeys.remove(preKeyId.toString());
+
+    await storage.write(key: preKey, value: jsonEncode(preKeys));
+  }
+
+  @override
+  Future<void> storePreKey(int preKeyId, PreKeyRecord record) async {
+    Map<String, dynamic> preKeys =
+        jsonDecode((await storage.read(key: preKey)).toString()) ?? {};
+
+    preKeys[preKeyId.toString()] = jsonEncode(record.serialize());
+
+    await storage.write(key: preKey, value: jsonEncode(preKeys));
+  }
+}

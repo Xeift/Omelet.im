a7ba826bbfd05249b1c64ee1899dbf953a198e69
Xeift <xeift.c@gmail.com>
2023-12-13 15:53:37 +0800

    minor fixes

--- client/sample_im/lib/onBtnPressed/on_test_btn2_pressed.dart
+++ client/sample_im/lib/onBtnPressed/on_test_btn2_pressed.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: avoid_print
-import './../signal_protocol/signal_protocol.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 Future<void> onTestBtnPressed2(Function updateHintMsg) async {

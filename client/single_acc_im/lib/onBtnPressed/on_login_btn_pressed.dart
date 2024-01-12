6169a4b9624b831d9b6ff76440bd907613e0b02b
Xeift <xeift.c@gmail.com>
2024-01-12 16:00:49 +0800

    minor fixes in on_login_btn_pressed.dart

--- client/single_acc_im/lib/onBtnPressed/on_login_btn_pressed.dart
+++ client/single_acc_im/lib/onBtnPressed/on_login_btn_pressed.dart
@@ -11,12 +11,13 @@ Future<void> onLoginBtnPressed(String username, String password,
     Function updateHintMsg, Function catHintMsg) async {
   if (await isJwtExsist()) {
     if (await isJwtValid()) {
-      print('jwt 存在且有效✅\n應跳轉至聊天室頁面');
+      print('jwt 存在且有效✅\n應跳轉至聊天室頁面\n');
     } else {
-      print('jwt 存在但無效❌\n應跳轉至登入頁面，提示使用者重新登入');
+      print('jwt 存在但無效❌\n應跳轉至登入頁面，提示使用者重新登入\n');
+      await login(username, password, updateHintMsg, catHintMsg);
     }
   } else {
-    print('jwt 不存在❌\n該使用者第一次開啟 App，應跳轉至登入頁面並產生公鑰包');
+    print('jwt 不存在❌\n該使用者第一次開啟 App，應跳轉至登入頁面並產生公鑰包\n');
+    await login(username, password, updateHintMsg, catHintMsg);
   }
-  await login(username, password, updateHintMsg, catHintMsg);
 }

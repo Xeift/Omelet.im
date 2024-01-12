a83c56203af02553c77782e5e8ad02968464b95d
Xeift <xeift.c@gmail.com>
2024-01-12 10:28:12 +0800

    add login func

--- client/single_acc_im/lib/onBtnPressed/on_login_btn_pressed.dart
+++ client/single_acc_im/lib/onBtnPressed/on_login_btn_pressed.dart
@@ -9,28 +9,32 @@ const storage = FlutterSecureStorage();
 
 Future<void> onLoginBtnPressed(String serverUri, String username,
     String password, Function updateHintMsg, Function catHintMsg) async {
-  if (await isJwtExsist()) {
-    print('jwt exsist!😎');
-  } else {
-    print('jwt not exsist!😂');
+  // if (await isJwtExsist()) {
+  //   print('jwt exsist!😎');
+  // } else {
+  //   print('jwt not exsist!😂');
+  // }
+
+  Future<void> login() async {
+    final res = await http.post(
+      Uri.parse('$serverUri/api/v1/login'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(
+          <String, String>{'username': username, 'password': password}),
+    );
+    const storage = FlutterSecureStorage();
+    final resBody = jsonDecode(res.body);
+
+    await storage.write(key: 'token', value: resBody['token']);
+    updateHintMsg(
+        '歡迎登入，${resBody["data"]["username"]}\n您的id為：${resBody["data"]["uid"]}');
   }
-  final res = await http.post(
-    Uri.parse('$serverUri/api/v1/login'),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(
-        <String, String>{'username': username, 'password': password}),
-  );
-  const storage = FlutterSecureStorage();
-  print(res.body);
-  final resBody = jsonDecode(res.body);
 
-  await storage.write(key: 'token', value: resBody['token']);
-  updateHintMsg(
-      '歡迎登入，${resBody["data"]["username"]}\n您的id為：${resBody["data"]["uid"]}');
+  await login();
 }
 
-Future<bool> isJwtExsist() async {
-  return (await storage.read(key: 'token')) != null;
-}
+// Future<bool> isJwtExsist() async {
+//   return (await storage.read(key: 'token')) != null;
+// }

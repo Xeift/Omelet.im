2e3259b7cf73479469fb037b0d93b3151884f04b
Xeift <xeift.c@gmail.com>
2024-01-12 16:07:33 +0800

    add save uid, username, email in login.dart

--- client/single_acc_im/lib/utils/login.dart
+++ client/single_acc_im/lib/utils/login.dart
@@ -17,6 +17,9 @@ Future<void> login(String username, String password, Function updateHintMsg,
   );
   final resBody = jsonDecode(res.body);
   await storage.write(key: 'token', value: resBody['token']);
+  await storage.write(key: 'uid', value: resBody['data']['uid']);
+  await storage.write(key: 'username', value: resBody['data']['username']);
+  await storage.write(key: 'email', value: resBody['data']['email']);
   updateHintMsg(
       '歡迎登入，${resBody["data"]["username"]}\n您的id為：${resBody["data"]["uid"]}');
 }

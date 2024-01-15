ab794c3639d67950b5431fca66e65cfa48863692
Xeift <xeift.c@gmail.com>
2024-01-15 19:47:18 +0800

    minor fixes in generate_and_store_key.dart

--- client/single_acc_im/lib/signal_protocol/generate_and_store_key.dart
+++ client/single_acc_im/lib/signal_protocol/generate_and_store_key.dart
@@ -1,21 +1,17 @@
 import 'dart:convert';
 
-import 'package:http/http.dart' as http;
 import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'safe_identity_store.dart';
 import 'safe_spk_store.dart';
 import 'safe_opk_store.dart';
-import './../utils/server_uri.dart';
 import './../api/post/upload_keys_api.dart';
 
 Future<void> generateAndStoreKey() async {
   const storage = FlutterSecureStorage();
 
   final selfUid = int.parse((await storage.read(key: 'uid')).toString());
-  // final selfAddress =
-  //     SignalProtocolAddress(selfUid.toString(), 1); // Signal protocol 地址
   final selfIpk = generateIdentityKeyPair(); // 產生身份金鑰對（長期金鑰對，平常不會動）
   final selfSpk = generateSignedPreKey(selfIpk, 0); // 產生 SPK（中期金鑰對，每 7 天更新一次）
   final selfOpks = generatePreKeys(0, 110); // 產生 OPK（短期金鑰對，1 則訊息會用掉 1 個）

b7484305856ed9d0b00aaebc0dd0c4ef8a42783b
Xeift <xeift.c@gmail.com>
2024-01-15 17:11:52 +0800

    finish pre key bundle type convert

--- client/single_acc_im/lib/onBtnPressed/on_send_msg_btn_pressed.dart
+++ client/single_acc_im/lib/onBtnPressed/on_send_msg_btn_pressed.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import './../store/safe_msg_store.dart';
+import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
+
+import './../store/safe_msg_store.dart';
 import './../utils/download_pre_key_bundle.dart';
 
 // ignore_for_file: avoid_print
@@ -11,7 +16,26 @@ Future<void> onSendMsgBtnPressed(
   const storage = FlutterSecureStorage();
   final token = await storage.read(key: 'token');
   final res = await downloadPreKeyBundleAPI(receiverId);
-  print(res.body);
+  final preKeyBundle = jsonDecode(res.body)['data'];
+  const opkId = '5';
+
+  final ipkPub = IdentityKey.fromBytes(
+      Uint8List.fromList(
+          jsonDecode(preKeyBundle['ipkPub']).cast<int>().toList()),
+      0);
+  final spkPub = Curve.decodePoint(
+      Uint8List.fromList(
+          jsonDecode(preKeyBundle['spkPub']).cast<int>().toList()),
+      0);
+  final spkSig = Uint8List.fromList(
+      jsonDecode(preKeyBundle['spkSig']).cast<int>().toList());
+  final opkPub = Curve.decodePoint(
+      Uint8List.fromList(
+          (jsonDecode(preKeyBundle['opkPub'][opkId])).cast<int>().toList()),
+      0);
+
+  print(opkPub);
+  print(opkPub.runtimeType);
 
   // print('[on_send_msg_btn_pressed.dart] $receiverId $msgContent');
 

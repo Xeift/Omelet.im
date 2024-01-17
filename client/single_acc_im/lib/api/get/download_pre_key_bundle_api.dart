8e60dec7527f421d60be1c16d809399ec7889a84
Xeift <xeift.c@gmail.com>
2024-01-17 21:18:32 +0800

    add randomChoice()

--- client/single_acc_im/lib/api/get/download_pre_key_bundle_api.dart
+++ client/single_acc_im/lib/api/get/download_pre_key_bundle_api.dart
@@ -1,11 +1,26 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../../utils/server_uri.dart';
+import './get_available_opk_index.dart';
+
+T randomChoice<T>(List<T> list) {
+  var random = Random();
+  return list[random.nextInt(list.length)];
+}
 
 Future<http.Response> downloadPreKeyBundleAPI(String uid) async {
   const storage = FlutterSecureStorage();
   final token = await storage.read(key: 'token');
+  final avaibleOpkIndex =
+      randomChoice(jsonDecode((await getAvailableOpkIndex(uid)).body)['data']);
+
+  print(avaibleOpkIndex);
+  print(avaibleOpkIndex.runtimeType);
+  print('--------------------------------');
 
   final res = await http.get(
       Uri.parse('$serverUri/api/v1/download-pre-key-bundle?uid=$uid'),
---
+++ client/single_acc_im/lib/api/get/get_available_opk_index.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../../utils/server_uri.dart';
+
+Future<http.Response> getAvailableOpkIndex(String uid) async {
+  const storage = FlutterSecureStorage();
+  final token = await storage.read(key: 'token');
+
+  final res = await http.get(
+      Uri.parse('$serverUri/api/v1/get-available-opk-index?uid=$uid'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token'
+      });
+  return res;
+}

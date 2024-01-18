b6597af63b531a7fb6fbc181c289167911234c95
Xeift <xeift.c@gmail.com>
2024-01-18 13:32:02 +0800

    move func to download_pre_key_bundle.dart

--- client/single_acc_im/lib/onBtnPressed/on_send_msg_btn_pressed.dart
+++ client/single_acc_im/lib/onBtnPressed/on_send_msg_btn_pressed.dart
@@ -1,40 +1,12 @@
-import 'dart:math';
-import 'dart:convert';
-import 'dart:typed_data';
-
-import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
-
-import '../api/get/download_pre_key_bundle_api.dart';
-import '../api/get/get_available_opk_index.dart';
+import './../signal_protocol/download_pre_key_bundle.dart';
 
 Future<void> onSendMsgBtnPressed(
     String receiverId, String msgContent, Function updateHintMsg) async {
-  final opkIndexRes = await getAvailableOpkIndex(receiverId);
-  final opkId = randomChoice(jsonDecode(opkIndexRes.body)['data']);
+  final (ipkPub, spkPub, spkSig, opkPub) =
+      await downloadPreKeyBundle(receiverId);
 
-  final res = await downloadPreKeyBundleAPI(receiverId, opkId);
-  final preKeyBundle = jsonDecode(res.body)['data'];
-
-  final ipkPub = IdentityKey.fromBytes(
-      Uint8List.fromList(
-          jsonDecode(preKeyBundle['ipkPub']).cast<int>().toList()),
-      0);
-  final spkPub = Curve.decodePoint(
-      Uint8List.fromList(
-          jsonDecode(preKeyBundle['spkPub']).cast<int>().toList()),
-      0);
-  final spkSig = Uint8List.fromList(
-      jsonDecode(preKeyBundle['spkSig']).cast<int>().toList());
-  final opkPub = Curve.decodePoint(
-      Uint8List.fromList(
-          (jsonDecode(preKeyBundle['opkPub'])).cast<int>().toList()),
-      0);
-
-  print(opkId);
-  print(opkPub.serialize());
-}
-
-T randomChoice<T>(List<T> list) {
-  var random = Random();
-  return list[random.nextInt(list.length)];
+  print(ipkPub);
+  print(spkPub);
+  print(spkSig);
+  print(opkPub);
 }
---
+++ client/single_acc_im/lib/signal_protocol/download_pre_key_bundle.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
+
+import '../api/get/download_pre_key_bundle_api.dart';
+import '../api/get/get_available_opk_index.dart';
+
+Future<(IdentityKey, ECPublicKey, Uint8List, ECPublicKey)> downloadPreKeyBundle(
+    String receiverId) async {
+  final opkIndexRes = await getAvailableOpkIndex(receiverId);
+  final opkId = randomChoice(jsonDecode(opkIndexRes.body)['data']);
+
+  final res = await downloadPreKeyBundleAPI(receiverId, opkId);
+  final preKeyBundle = jsonDecode(res.body)['data'];
+
+  final ipkPub = IdentityKey.fromBytes(
+      Uint8List.fromList(
+          jsonDecode(preKeyBundle['ipkPub']).cast<int>().toList()),
+      0);
+  final spkPub = Curve.decodePoint(
+      Uint8List.fromList(
+          jsonDecode(preKeyBundle['spkPub']).cast<int>().toList()),
+      0);
+  final spkSig = Uint8List.fromList(
+      jsonDecode(preKeyBundle['spkSig']).cast<int>().toList());
+  final opkPub = Curve.decodePoint(
+      Uint8List.fromList(
+          (jsonDecode(preKeyBundle['opkPub'])).cast<int>().toList()),
+      0);
+
+  return (ipkPub, spkPub, spkSig, opkPub);
+}
+
+T randomChoice<T>(List<T> list) {
+  var random = Random();
+  return list[random.nextInt(list.length)];
+}

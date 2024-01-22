a2c0c9adf0223e840252f00ce40381d0e1b19867
Xeift <xeift.c@gmail.com>
2024-01-22 10:23:56 +0800

    minor fixes in encrypt_message.dart

--- client/single_acc_im/lib/signal_protocol/encrypt_message.dart
+++ client/single_acc_im/lib/signal_protocol/encrypt_message.dart
@@ -3,11 +3,10 @@ import 'dart:typed_data';
 
 import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
 
-import './../signal_protocol/download_pre_key_bundle.dart';
-import './../signal_protocol/safe_identity_store.dart';
 import './../signal_protocol/safe_spk_store.dart';
 import './../signal_protocol/safe_opk_store.dart';
 import './../signal_protocol/safe_session_store.dart';
+import './../signal_protocol/safe_identity_store.dart';
 
 Future<String> encryptMessage(
     IdentityKey ipkPub,
@@ -16,21 +15,14 @@ Future<String> encryptMessage(
     ECPublicKey opkPub,
     int spkId,
     int opkId,
-    String receiverId,
+    String remoteUid,
     String msgContent,
     Function updateHintMsg) async {
-  print(ipkPub);
-  print(spkPub);
-  print(spkSig);
-  print(opkPub);
-  print(opkId);
-  print(spkId);
-
   final ipkStore = SafeIdentityKeyStore();
   final registrationId = await ipkStore.getLocalRegistrationId();
   final spkStore = SafeSpkStore();
   final opkStore = SafeOpkStore();
-  final remoteAddress = SignalProtocolAddress(receiverId.toString(), 1);
+  final remoteAddress = SignalProtocolAddress(remoteUid.toString(), 1);
 
   // 建立 SessionStore
   final sessionStore = SafeSessionStore();

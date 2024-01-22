caa50b3d28e9e3a80dc8fe923105b70ee99989e0
Xeift <xeift.c@gmail.com>
2024-01-22 11:17:42 +0800

    remove unuse code

--- client/single_acc_im/lib/utils/jwt.dart
+++ client/single_acc_im/lib/utils/jwt.dart
@@ -1,18 +1,7 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-// import './../api/post/check_jwt_status_api.dart';
-
 const storage = FlutterSecureStorage();
 
 Future<bool> isJwtExsist() async {
   return (await storage.read(key: 'token')) != null;
 }
-
-// Future<bool> isJwtValid() async {
-//   final res = await checkJwtStatusAPI();
-//   if (res.statusCode == 200) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }

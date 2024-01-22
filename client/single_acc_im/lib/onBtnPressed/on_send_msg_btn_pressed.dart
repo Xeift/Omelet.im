2c735b73b8949a335c56c0cf2f52bf92f58ccee4
Xeift <xeift.c@gmail.com>
2024-01-22 10:01:57 +0800

    minor fixes in on_send_msg_btn_pressed.dart

--- client/single_acc_im/lib/onBtnPressed/on_send_msg_btn_pressed.dart
+++ client/single_acc_im/lib/onBtnPressed/on_send_msg_btn_pressed.dart
@@ -1,26 +1,25 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-import './../signal_protocol/download_pre_key_bundle.dart';
-import './../signal_protocol/encrypt_message.dart';
-import '../main.dart' show socket;
+import './../main.dart' show socket;
 import './../message/safe_msg_store.dart';
+import './../signal_protocol/encrypt_message.dart';
+import './../signal_protocol/download_pre_key_bundle.dart';
 
 Future<void> onSendMsgBtnPressed(
     String remoteUid, String msgContent, Function updateHintMsg) async {
+  const storage = FlutterSecureStorage();
+  final selfUid = await storage.read(key: 'uid');
+  final currentTimestamp = DateTime.now().millisecondsSinceEpoch.toString();
+
+  // 準備對方的 Pre Key Bundle
   final (ipkPub, spkPub, spkSig, opkPub, spkId, opkId) =
       await downloadPreKeyBundle(remoteUid);
 
+  // 加密訊息
   final cihertext = await encryptMessage(ipkPub, spkPub, spkSig, opkPub, spkId,
       opkId, remoteUid, msgContent, updateHintMsg);
 
-  print('加密訊息：$cihertext');
-
-  const storage = FlutterSecureStorage();
-  final selfUid = await storage.read(key: 'uid');
-
-  final currentTimestamp = DateTime.now().millisecondsSinceEpoch.toString();
-
-  // sent msg
+  // 發送訊息
   socket.emit('clientSendMsgToServer', {
     'timestamp': currentTimestamp,
     'type': 'text',
@@ -32,7 +31,7 @@ Future<void> onSendMsgBtnPressed(
     'opkId': opkId
   });
 
-  // store msg sent
+  // 儲存發送的訊息
   final safeMsgStore = SafeMsgStore();
   safeMsgStore.writeMsg(remoteUid, {
     'timestamp': currentTimestamp,

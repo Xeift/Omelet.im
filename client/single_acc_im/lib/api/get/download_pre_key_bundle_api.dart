fe2a267ff5032d59eb249e71dd7ef650d01f2ea0
Xeift <xeift.c@gmail.com>
2024-01-22 09:25:05 +0800

    minor fixes in download_pre_key_bundle_api.dart

--- client/single_acc_im/lib/api/get/download_pre_key_bundle_api.dart
+++ client/single_acc_im/lib/api/get/download_pre_key_bundle_api.dart
@@ -3,13 +3,14 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../../utils/server_uri.dart';
 
-Future<http.Response> downloadPreKeyBundleAPI(String uid, String opkId) async {
+Future<http.Response> downloadPreKeyBundleAPI(
+    String remoteUid, String remoteOpkId) async {
   const storage = FlutterSecureStorage();
   final token = await storage.read(key: 'token');
 
   final res = await http.get(
       Uri.parse(
-          '$serverUri/api/v1/download-pre-key-bundle?uid=$uid&opkId=$opkId'),
+          '$serverUri/api/v1/download-pre-key-bundle?uid=$remoteUid&opkId=$remoteOpkId'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization': 'Bearer $token'

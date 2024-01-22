d7a836a37712205df74fc2f8f40a307cc95a137b
Xeift <xeift.c@gmail.com>
2024-01-22 09:34:21 +0800

    minor fixes in fet_svailable_opk_indenx_api.dart

--- client/single_acc_im/lib/api/get/get_available_opk_index_api.dart
+++ client/single_acc_im/lib/api/get/get_available_opk_index_api.dart
@@ -3,12 +3,12 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../../utils/server_uri.dart';
 
-Future<http.Response> getAvailableOpkIndexApi(String uid) async {
+Future<http.Response> getAvailableOpkIndexApi(String remoteUid) async {
   const storage = FlutterSecureStorage();
   final token = await storage.read(key: 'token');
 
   final res = await http.get(
-      Uri.parse('$serverUri/api/v1/get-available-opk-index?uid=$uid'),
+      Uri.parse('$serverUri/api/v1/get-available-opk-index?uid=$remoteUid'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization': 'Bearer $token'

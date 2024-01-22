228740107561b0cfb3d5c28babb513395bafcb7f
Xeift <xeift.c@gmail.com>
2024-01-22 09:44:09 +0800

    rename var in safe_msg_store

--- client/single_acc_im/lib/message/safe_msg_store.dart
+++ client/single_acc_im/lib/message/safe_msg_store.dart
@@ -5,38 +5,38 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class SafeMsgStore {
   final storage = const FlutterSecureStorage();
 
-  String uidToKey(String friendUid, int index) {
-    return 'msg_${friendUid}_$index';
+  String uidToKey(String remoteUid, int index) {
+    return 'msg_${remoteUid}_$index';
   }
 
-  Future<void> writeMsg(String friendUid, Map<String, dynamic> msg) async {
+  Future<void> writeMsg(String remoteUid, Map<String, dynamic> msg) async {
     final msgJson = jsonEncode(msg);
-    String key = uidToKey(friendUid, await getMsgCount(friendUid) + 1);
+    String key = uidToKey(remoteUid, await getMsgCount(remoteUid) + 1);
     await storage.write(key: key, value: msgJson);
   }
 
-  Future<Map<String, dynamic>> readMsg(String friendUid, int index) async {
-    String key = uidToKey(friendUid, index);
+  Future<Map<String, dynamic>> readMsg(String remoteUid, int index) async {
+    String key = uidToKey(remoteUid, index);
     return jsonDecode((await storage.read(key: key)).toString());
   }
 
-  Future<void> deleteMsg(String friendUid, int index) async {
-    String key = uidToKey(friendUid, index);
+  Future<void> deleteMsg(String remoteUid, int index) async {
+    String key = uidToKey(remoteUid, index);
     await storage.delete(key: key);
   }
 
-  Future<int> getMsgCount(String friendUid) async {
+  Future<int> getMsgCount(String remoteUid) async {
     Map<String, String> allData = await storage.readAll();
     List<String> filteredKeys = allData.keys
-        .where((key) => key.startsWith('msg_${friendUid}_'))
+        .where((key) => key.startsWith('msg_${remoteUid}_'))
         .toList();
     return filteredKeys.length;
   }
 
-  Future<List<String>> readAllMsg(String friendUid) async {
+  Future<List<String>> readAllMsg(String remoteUid) async {
     Map<String, String> allData = await storage.readAll();
     List<String> filteredKeys = allData.keys
-        .where((key) => key.startsWith('msg_${friendUid}_'))
+        .where((key) => key.startsWith('msg_${remoteUid}_'))
         .toList();
     List<String> messages = [];
     for (String key in filteredKeys) {

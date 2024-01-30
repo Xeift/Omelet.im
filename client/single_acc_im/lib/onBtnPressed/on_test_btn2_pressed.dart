a8603387c8e0bd4d46beb266cd50459ad2b469b5
Xeift <xeift.c@gmail.com>
2024-01-30 08:58:41 +0800

    minor fixes in on_msg_btn2_pressed.dart

--- client/single_acc_im/lib/onBtnPressed/on_test_btn2_pressed.dart
+++ client/single_acc_im/lib/onBtnPressed/on_test_btn2_pressed.dart
@@ -2,11 +2,11 @@
 
 import './../signal_protocol/safe_session_store.dart';
 import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
-import 'dart:developer' as logDev; // TODO:
+import 'dart:developer' as log_dev; // TODO:
 
 Future<void> onTestBtn2Pressed(Function updateHintMsg) async {
   final safeSessionStore = SafeSessionStore();
   const remoteAddress = SignalProtocolAddress('492312533160431617', 1);
   final sess = await safeSessionStore.loadSession(remoteAddress);
-  logDev.log(sess.serialize().toString());
+  log_dev.log(sess.serialize().toString());
 }

eb9f54e0711f841ebad278bad9747fec80dcc491
Xeift <xeift.c@gmail.com>
2024-02-04 23:57:03 +0800

    fix uri does not exsist error

--- client/omelet/lib/pages/login_signup/forget_page.dart
+++ client/omelet/lib/pages/login_signup/forget_page.dart
@@ -1,19 +1,19 @@
-
 // ignore_for_file: library_private_types_in_public_api, non_constant_identifier_names, prefer_typing_uninitialized_variables, use_build_context_synchronously, unnecessary_string_interpolations, avoid_print, duplicate_ignore, deprecated_member_use
 
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:omelet/api/all_login_api.dart';
 import 'package:omelet/componets/alert/AlertMsg.dart';
+
+import './../../api/post/reset_password_api.dart';
 
 class ForgetPage extends StatefulWidget {
   const ForgetPage({Key? key}) : super(key: key);
   @override
-   _ForgetPageState createState() =>  _ForgetPageState();
+  _ForgetPageState createState() => _ForgetPageState();
 }
 
-class _ForgetPageState extends State<ForgetPage>{
+class _ForgetPageState extends State<ForgetPage> {
   late String _ForgetEmail = '';
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   var forgetEamilcontroller;
@@ -22,17 +22,17 @@ class _ForgetPageState extends State<ForgetPage>{
     super.initState();
     forgetEamilcontroller = TextEditingController();
   }
+
   @override
-  Widget build(BuildContext context){
-    
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:Colors.white,
+        backgroundColor: Colors.white,
         elevation: 0,
         leading: IconButton(
-          icon:const Icon(Icons.arrow_back_ios,color:Colors.black),
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
           onPressed: () => Navigator.pop(context),
-          ),
+        ),
       ),
       body: Form(
         key: _formKey,
@@ -43,72 +43,70 @@ class _ForgetPageState extends State<ForgetPage>{
             const SizedBox(height: kToolbarHeight),
             const SizedBox(height: 30),
             buildForget_title(),
-            const SizedBox(height:120),
+            const SizedBox(height: 120),
             buildForget_emailTextField(),
-            const SizedBox(height:30),
+            const SizedBox(height: 30),
             buildForget_submitButton(),
-          
           ],
         ),
       ),
     );
   }
 
-  Widget buildForget_title(){
+  Widget buildForget_title() {
     return const Padding(
       padding: EdgeInsets.all(8),
-      child:Text(
-          'Forget Password',
-          style: TextStyle(fontSize: 40),
+      child: Text(
+        'Forget Password',
+        style: TextStyle(fontSize: 40),
       ),
     );
   }
 
-  Widget buildForget_emailTextField(){
-  return TextField(
-    decoration: const InputDecoration(
-      border: OutlineInputBorder(),
-      labelText: 'Email Address',
-    ),
-    controller: forgetEamilcontroller,
-  );
+  Widget buildForget_emailTextField() {
+    return TextField(
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Email Address',
+      ),
+      controller: forgetEamilcontroller,
+    );
   }
-  Widget buildForget_submitButton(){
+
+  Widget buildForget_submitButton() {
     return Align(
-      child:SizedBox(
-         height: 50,
+      child: SizedBox(
+        height: 50,
         width: 150,
-        child: ElevatedButton( 
+        child: ElevatedButton(
           style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(Colors.black),
+            backgroundColor: MaterialStateProperty.all(Colors.black),
           ),
           child: Text(
             'Submit',
             style: Theme.of(context).primaryTextTheme.headline6,
           ),
-          onPressed: () async{
+          onPressed: () async {
             _ForgetEmail = forgetEamilcontroller.text;
-            final forgetemailres = await forgetemailAPI(_ForgetEmail);
+            final forgetemailres = await resetPasswordSendMailAPI(_ForgetEmail);
             final statusCode = forgetemailres.statusCode;
             final resBody = jsonDecode(forgetemailres.body);
-            if(statusCode == 200){
-              LoginEorroMsg(context,'email 已成功寄出');
-            }else if(statusCode == 401){
+            if (statusCode == 200) {
+              LoginEorroMsg(context, 'email 已成功寄出');
+            } else if (statusCode == 401) {
               LoginEorroMsg(context, 'Eamil 不存在，請先註冊');
-            }else if(statusCode == 500){
+            } else if (statusCode == 500) {
               LoginEorroMsg(context, 'Eorror server');
             }
 
-
             print('$_ForgetEmail');
             print(statusCode); // http 狀態碼
-            print(resBody); // 
+            print(resBody); //
             print(resBody['message']); // 取得登入 API 回應內容中的 message 內容
-             //build await function after complet date transform
+            //build await function after complet date transform
           },
-          ),
+        ),
       ),
     );
   }
 }
-
--- client/omelet/lib/pages/login_signup/sign_up_page.dart
+++ client/omelet/lib/pages/login_signup/sign_up_page.dart
@@ -4,79 +4,82 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
-
-import '../../api/all_login_api.dart';
 import '../../componets/alert/AlertMsg.dart';
 
+import './../../api/post/signup_api.dart';
 
-
-class SignUpPage extends StatefulWidget{
-  const SignUpPage({Key? key}):super(key:key);
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
   @override
   _SignUpPageState createState() => _SignUpPageState();
 }
-class _SignUpPageState extends State<SignUpPage>{
+
+class _SignUpPageState extends State<SignUpPage> {
   Color _eyeColor = Colors.grey;
   bool _isObscure = true;
-  late String _signUpEamil ='',_singUppassword = '',_signUpName = '';
+  late String _signUpEamil = '', _singUppassword = '', _signUpName = '';
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  var emailSignUpTextFieldController,passwordSignUpTextFieldController,nameSignUpTextFieldController;
+  var emailSignUpTextFieldController,
+      passwordSignUpTextFieldController,
+      nameSignUpTextFieldController;
   @override
-  void initState(){
+  void initState() {
     super.initState();
     emailSignUpTextFieldController = TextEditingController();
     passwordSignUpTextFieldController = TextEditingController();
     nameSignUpTextFieldController = TextEditingController();
   }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        backgroundColor:Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
         elevation: 0,
-        leading: IconButton(icon: const Icon(Icons.arrow_back_ios,color:Colors.black),
-        onPressed: ()=>Navigator.pop(context),),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
       ),
       body: Form(
-        key:_formKey,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        child:ListView(
-          padding:const EdgeInsets.symmetric(horizontal: 20),
-          children: [
-            const SizedBox(height: kToolbarHeight),
-            const SizedBox(height: 50),
-            buildTitle(),
-            const SizedBox(height: 30),
-            buildSignUpEamilTextField(),
-            const SizedBox(height: 30),
-            buildSignUpNameTextField(),
-            const SizedBox(height: 30),
-            buildSignUpPasswordTextField(),
-            const SizedBox(height:30),
-            buildSignUpButton(),
+          key: _formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            children: [
+              const SizedBox(height: kToolbarHeight),
+              const SizedBox(height: 50),
+              buildTitle(),
+              const SizedBox(height: 30),
+              buildSignUpEamilTextField(),
+              const SizedBox(height: 30),
+              buildSignUpNameTextField(),
+              const SizedBox(height: 30),
+              buildSignUpPasswordTextField(),
+              const SizedBox(height: 30),
+              buildSignUpButton(),
+            ],
+          )),
+    );
+  }
 
-          ],
-        )
+  Widget buildSignUpEamilTextField() {
+    return TextField(
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Email Address',
       ),
-    
+      controller: emailSignUpTextFieldController,
     );
   }
-  Widget buildSignUpEamilTextField(){
+
+  Widget buildSignUpNameTextField() {
     return TextField(
       decoration: const InputDecoration(
-      border: OutlineInputBorder(),
-      labelText: 'Email Address',
-    ),
-    controller: emailSignUpTextFieldController,
-    );
-  }
-  Widget buildSignUpNameTextField(){
-    return TextField(
-      decoration: const InputDecoration(
-      border: OutlineInputBorder(),
-      labelText: 'User Name',
-    ),
-    controller: nameSignUpTextFieldController,
+        border: OutlineInputBorder(),
+        labelText: 'User Name',
+      ),
+      controller: nameSignUpTextFieldController,
     );
   }
 
@@ -86,7 +89,7 @@ class _SignUpPageState extends State<SignUpPage>{
       obscureText: _isObscure,
       controller: passwordSignUpTextFieldController,
       decoration: InputDecoration(
-        border:OutlineInputBorder(),
+        border: OutlineInputBorder(),
         //密碼遮蔽button
         labelText: 'Password',
         suffixIcon: IconButton(
@@ -107,6 +110,7 @@ class _SignUpPageState extends State<SignUpPage>{
       ),
     );
   }
+
   Widget buildTitle() {
     //Login字樣
     return const Padding(
@@ -117,7 +121,8 @@ class _SignUpPageState extends State<SignUpPage>{
       ),
     );
   }
-    Widget buildSignUpButton() {
+
+  Widget buildSignUpButton() {
     return Align(
       child: SizedBox(
         height: 50,
@@ -129,35 +134,33 @@ class _SignUpPageState extends State<SignUpPage>{
             side: MaterialStateProperty.all(
                 const BorderSide(color: Colors.grey, width: 1)),
           ),
-          onPressed: () async{
+          onPressed: () async {
             _signUpEamil = emailSignUpTextFieldController.text;
             _singUppassword = passwordSignUpTextFieldController.text;
             _signUpName = nameSignUpTextFieldController.text;
-            final signUpres = await signUpAPI(_signUpEamil,_signUpName,_singUppassword);
+            final signUpres =
+                await signupAPI(_signUpEamil, _signUpName, _singUppassword);
             final signUpstateCode = signUpres.statusCode;
             final resBody = jsonDecode(signUpres.body);
             print('$_signUpEamil');
             print(signUpstateCode); // http 狀態碼
             print(resBody); // 登入 API 回應內容
             print(resBody['message']);
-            
+
             if (signUpstateCode == 200) {
               print('登入成功( •̀ ω •́ )✧');
               // 這裡要寫登入成功時的邏輯，比如提示使用者密碼錯誤
               // 所有 API 回應內容請見：Omelet.im\backend\api\login.js
             } else if (signUpstateCode == 401) {
               // 帳號密碼錯誤
-              LoginEorroMsg(context,'Eamil已存在');
+              LoginEorroMsg(context, 'Eamil已存在');
             } else if (signUpstateCode == 422) {
               // 帳號密碼為空
-              LoginEorroMsg(context,'請輸入註冊資訊');
-            } 
-            else if (signUpstateCode == 500) {
+              LoginEorroMsg(context, '請輸入註冊資訊');
+            } else if (signUpstateCode == 500) {
               // 後端其他錯誤
-              LoginEorroMsg(context,'Another Eorro for server');
+              LoginEorroMsg(context, 'Another Eorro for server');
             }
-
-            
           },
           autofocus: true,
           child: Text(

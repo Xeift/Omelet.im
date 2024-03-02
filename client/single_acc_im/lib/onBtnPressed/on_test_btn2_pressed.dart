adedd1b53d64d178ecb5f1f7f47e2ca9b21f736f
Xeift <xeift.c@gmail.com>
2024-03-02 20:42:31 +0800

    minor fixes

--- client/single_acc_im/lib/onBtnPressed/on_test_btn2_pressed.dart
+++ client/single_acc_im/lib/onBtnPressed/on_test_btn2_pressed.dart
@@ -1,22 +1,16 @@
 // ignore_for_file: avoid_print
 import './../api/post/update_pfp.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:convert';
-import 'dart:io';
 
 Future<void> onTestBtn2Pressed(Function updateHintMsg) async {
   final picker = ImagePicker();
   var image = await picker.pickImage(source: ImageSource.gallery);
 
   if (image != null) {
-    // final bytes = await File(image.path).readAsBytes();
-    // final img64 = base64Encode(bytes);
-    // final res = await updatePfp(img64);
-    // final imgJson = jsonEncode(bytes);
-    // final res = await updatePfp(imgJson);
-
     final res = await updatePfp(image.path);
-    print(res.statusCode);
+    if (res.statusCode == 200) {
+      print('[on_test_btn2Pressed.dart] update pfp successfully');
+    }
   } else {
     print('No image selected.');
   }

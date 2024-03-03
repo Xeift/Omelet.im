1edc091a4242ce5a2caf57cd3dc6f1b19105169b
Xeift <xeift.c@gmail.com>
2024-03-03 09:06:45 +0800

    remove pfp `upload` folder

--- backend/api/post/updatePfp.js
+++ backend/api/post/updatePfp.js
@@ -3,17 +3,15 @@ const router = express.Router();
 const jwt = require('../../utils/jwt.js');
 const authController = require('../../controller/authController.js');
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer();
 const fs = require('fs');
 
 router.post('/', jwt.verifyJWT, upload.single('pfpData'), async(req, res) => {
     let decodedToken = req.decodedToken;
     let ourUid = decodedToken._uid;
 
-    let pfpData = req.file; // multer 會將上傳的檔案放在 req.file
-    let img = fs.readFileSync(pfpData.path);
-    let encodedImage = img.toString('base64');
-    await authController.updatePfpByUid(ourUid, encodedImage);
+    const imageBase64 = Buffer.from(req.file.buffer).toString('base64');
+    await authController.updatePfpByUid(ourUid, imageBase64);
 
     try {
         res.status(200).json({

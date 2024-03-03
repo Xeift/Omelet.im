2ce79d8e497262c71bc622c50f367859ffc8eb9f
Maple <mapleiv2718@gmail.com>
2024-03-04 01:44:53 +0800

    bottomNBar&message

--- client/omelet/lib/pages/chat_list_page.dart
+++ client/omelet/lib/pages/chat_list_page.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:omelet/pages/notification_page.dart';
 
 import '../pages/setting/setting_page.dart';
-import '../componets/Screen/home_page_botton_navbar.dart';
+import '../theme/theme_constants.dart';
+import '../pages/message_page.dart';
+import '../pages/notification_page.dart';
 
-class ChatListPage extends StatelessWidget {
+class ChatListPage extends StatefulWidget {
   const ChatListPage({Key? key}) : super(key: key);
 
   @override
+  State<ChatListPage> createState() => _ChatListPageState();
+}
+
+class _ChatListPageState extends State<ChatListPage> {
+  final ValueNotifier<int> pageIndex = ValueNotifier(1);
+
+  final List<Widget> pages = const [
+    MessagePage(),
+    NotificationPgae(),
+    SettingPage(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
+    pageIndex.addListener(() {
+      print(pageIndex.value);
+    });
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
@@ -26,12 +45,121 @@ class ChatListPage extends StatelessWidget {
         leading: const Icon(Icons.account_circle),
         backgroundColor: const Color.fromARGB(255, 0, 0, 0).withAlpha(30),
       ),
-      body:const SafeArea(
-        child: Stack(
-          fit: StackFit.expand,
+      body: ValueListenableBuilder(
+        valueListenable: pageIndex,
+        builder: (BuildContext context, int value, _) {
+          return pages[value];
+        },
+      ),
+      bottomNavigationBar: BottonNavbar(
+        onItemSelected: (index) {
+          setState(() {
+            pageIndex.value = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class BottonNavbar extends StatefulWidget {
+  const BottonNavbar({Key? key, required this.onItemSelected}) : super(key: key);
+
+  final ValueChanged<int> onItemSelected;
+
+  @override
+  State<BottonNavbar> createState() => _BottonNavbarState();
+}
+
+class _BottonNavbarState extends State<BottonNavbar> {
+  var selectedIndex = 0;
+  
+  void handleItemSelected(int index){
+    setState(() {
+      selectedIndex = index;
+    });
+
+    widget.onItemSelected(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context); // 获取当前上下文中的主题
+
+    return SafeArea(
+      child: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          color: theme.bottomNavigationBarTheme.backgroundColor, // 应用底部导航栏的背景颜色
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.3), // 阴影颜色
+              blurRadius: 5, // 模糊半径
+              offset: const Offset(0, -3), // 阴影偏移量，使其向上
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            NavBarItem(
+              index: 0, 
+              label: 'Message', 
+              icon: Icons.message, 
+              isSelected: (selectedIndex == 0),
+              onTap: handleItemSelected),
+            NavBarItem(
+              index: 1, 
+              label: 'Notification', 
+              icon: Icons.notifications, 
+              isSelected: (selectedIndex == 1),
+              onTap: handleItemSelected),
+            NavBarItem(
+              index: 2, 
+              label: 'Setting', 
+              icon: Icons.settings, 
+              isSelected: (selectedIndex == 2),
+              onTap: handleItemSelected),
+          ],
         ),
       ),
-      bottomNavigationBar: const BottonNavbar(),
+    );
+  }
+}
+
+class NavBarItem extends StatelessWidget {
+  const NavBarItem({Key? key, required this.label, required this.icon, required this.index, required this.onTap,this.isSelected = false}) : super(key: key);
+
+  final ValueChanged<int> onTap;
+  final int index;
+  final bool isSelected;
+  final String label;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => onTap(index),
+      child: SizedBox(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              size: 20,
+              color: isSelected ? AppColor.secondary : null,
+              ),
+            const SizedBox(height: 7,),
+            Text(
+              label,
+              style: isSelected 
+              ? const TextStyle(fontSize: 11,color:AppColor.secondary)
+              : const TextStyle(fontSize: 11,color:Color.fromARGB(255, 255, 255, 255)),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ client/omelet/lib/pages/message_page.dart
@@ -0,0 +1,91 @@
+import 'package:faker/faker.dart';
+import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
+import 'package:omelet/componets/message/avatar.dart';
+
+import '../models/message_data.dart';
+import '../helpers.dart';
+
+
+
+class MessagePage extends StatefulWidget {
+  const MessagePage({Key? key}) : super(key: key);
+
+  @override
+  State<MessagePage> createState() => _MessagePageState();
+}
+
+class _MessagePageState extends State<MessagePage> {
+  @override
+
+
+  @override
+  Widget build(BuildContext context) {
+    //bool isDark = Theme.of(context).brightness == Brightness.dark;
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(_delegate)
+        ),
+      ],
+    );
+  }
+  Widget _delegate(BuildContext context, int index) {
+
+    final Faker faker = Faker();
+    final date = Helpers.randomDate();
+    
+    return MessageTitle(
+      messageData: MessageData(
+        senderName: faker.person.name(),
+        message:faker.lorem.sentence(),
+        messageDate: date,
+        dateMessage: Jiffy.parse('1997/09/23').fromNow(),
+        profilePicture: Helpers.randomPictureUrl(),
+      )
+    );
+  }
+}
+
+class MessageTitle extends StatelessWidget {
+  const MessageTitle({Key? key, required this.messageData}) : super(key: key);
+
+  final MessageData messageData;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child:Avatar.medium(url: messageData.profilePicture),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(messageData.senderName,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                letterSpacing:0.2,
+                wordSpacing: 1.5,
+                fontWeight: FontWeight.w900,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                child: Text(
+                  messageData.message,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontSize: 12,
+                    color: Color.fromARGB(255, 162, 162, 162),
+                  ),),
+              )
+            ],
+        ))
+      ],
+    );
+  }
+}

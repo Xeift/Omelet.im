d5e004eaa300b861ec22b263fb2f00092e99db2c
Xeift <xeift.c@gmail.com>
2024-03-09 13:24:22 +0800

    make checkOpkStatus() and checkSpkStatus()

---
+++ client/test_im_v4/lib/utils/check_spk_status.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
+import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
+
+import 'package:test_im_v4/api/post/update_spk_api.dart';
+import 'package:test_im_v4/api/get/get_self_spk_status_api.dart';
+
+import 'package:test_im_v4/signal_protocol/safe_spk_store.dart';
+import 'package:test_im_v4/signal_protocol/safe_identity_store.dart';
+
+Future<void> checkSpkStatus() async {
+  final getSelfSpkStatusRes = await getSelfSpkStatusApi();
+  final getSelfSpkStatusResBody = jsonDecode(getSelfSpkStatusRes.body);
+  print('[main.dart] getSelfSpkStatusResBody: $getSelfSpkStatusResBody');
+  final spkStatus = getSelfSpkStatusResBody['data'];
+  final spkExpired = spkStatus['spkExpired'];
+  final lastBatchSpkId = spkStatus['lastBatchSpkId'];
+
+  if (spkExpired) {
+    final ipkStore = SafeIdentityKeyStore();
+    final selfIpk = await ipkStore.getIdentityKeyPair();
+    final newSpk = generateSignedPreKey(selfIpk, lastBatchSpkId + 1);
+
+    final res = await updateSpkApi(
+      jsonEncode({
+        newSpk.id.toString():
+            jsonEncode(newSpk.getKeyPair().publicKey.serialize())
+      }),
+      jsonEncode({newSpk.id.toString(): jsonEncode(newSpk.signature)}),
+    );
+    print('[main.dart] 更新 SPK👉 ${res.body}');
+
+    final spkStore = SafeSpkStore();
+    await spkStore.storeSignedPreKey(newSpk.id, newSpk);
+  }
+}
--- client/test_im_v4/lib/utils/init_socket.dart
+++ client/test_im_v4/lib/utils/init_socket.dart
@@ -3,21 +3,15 @@
 import 'dart:convert';
 
 import 'package:socket_io_client/socket_io_client.dart' as io;
-import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
 
 import 'package:test_im_v4/utils/jwt.dart';
 import 'package:test_im_v4/utils/load_local_info.dart';
 import 'package:test_im_v4/utils/login.dart';
+import 'package:test_im_v4/utils/check_opk_status.dart';
+import 'package:test_im_v4/utils/check_spk_status.dart';
 
-import 'package:test_im_v4/api/post/update_opk_api.dart';
-import 'package:test_im_v4/api/post/update_spk_api.dart';
 import 'package:test_im_v4/api/get/get_unread_msg_api.dart';
-import 'package:test_im_v4/api/get/get_self_opk_status_api.dart';
-import 'package:test_im_v4/api/get/get_self_spk_status_api.dart';
 
-import 'package:test_im_v4/signal_protocol/safe_opk_store.dart';
-import 'package:test_im_v4/signal_protocol/safe_spk_store.dart';
-import 'package:test_im_v4/signal_protocol/safe_identity_store.dart';
 import 'package:test_im_v4/signal_protocol/generate_and_store_key.dart';
 
 import 'package:test_im_v4/message/safe_msg_store.dart';
@@ -43,58 +37,10 @@ Future<void> initSocket() async {
         print('--------------------------------\n');
 
         // 若伺服器中自己的 OPK 耗盡，則產生並上傳 OPK
-        final getSelfOpkStatusRes = await getSelfOpkStatusApi();
-        final getSelfOpkStatusResBody = jsonDecode(getSelfOpkStatusRes.body);
-        print(
-            '[main.dart] getSelfOpkStatusResBody 內容：$getSelfOpkStatusResBody');
-
-        final outOfOpk = getSelfOpkStatusResBody['data']['outOfOpk'];
-        final lastBatchMaxOpkId =
-            getSelfOpkStatusResBody['data']['lastBatchMaxOpkId'];
-
-        if (outOfOpk) {
-          final newOpks = generatePreKeys(lastBatchMaxOpkId + 1, 100);
-
-          final res = await updateOpkApi(jsonEncode({
-            for (var newOpk in newOpks)
-              newOpk.id.toString():
-                  jsonEncode(newOpk.getKeyPair().publicKey.serialize())
-          }));
-          print('--------------------------------');
-          print('[main.dart] new opk👉 ${res.body}');
-          print('--------------------------------\n');
-
-          final opkStore = SafeOpkStore();
-          for (final newOpk in newOpks) {
-            await opkStore.storePreKey(newOpk.id, newOpk);
-          }
-        }
+        await checkOpkStatus();
 
         // 若伺服器中自己的 SPK 期限已到（7 天），則產生並上傳 SPK
-        final getSelfSpkStatusRes = await getSelfSpkStatusApi();
-        final getSelfSpkStatusResBody = jsonDecode(getSelfSpkStatusRes.body);
-        print('[main.dart] getSelfSpkStatusResBody: $getSelfSpkStatusResBody');
-        final spkStatus = getSelfSpkStatusResBody['data'];
-        final spkExpired = spkStatus['spkExpired'];
-        final lastBatchSpkId = spkStatus['lastBatchSpkId'];
-
-        if (spkExpired) {
-          final ipkStore = SafeIdentityKeyStore();
-          final selfIpk = await ipkStore.getIdentityKeyPair();
-          final newSpk = generateSignedPreKey(selfIpk, lastBatchSpkId + 1);
-
-          final res = await updateSpkApi(
-            jsonEncode({
-              newSpk.id.toString():
-                  jsonEncode(newSpk.getKeyPair().publicKey.serialize())
-            }),
-            jsonEncode({newSpk.id.toString(): jsonEncode(newSpk.signature)}),
-          );
-          print('[main.dart] 更新 SPK👉 ${res.body}');
-
-          final spkStore = SafeSpkStore();
-          await spkStore.storeSignedPreKey(newSpk.id, newSpk);
-        }
+        await checkSpkStatus();
 
         // 取得未讀訊息
         final getUnreadMsgAPIRes = await getUnreadMsgAPI();

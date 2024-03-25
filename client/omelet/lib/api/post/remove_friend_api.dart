1c55771bdcf7191e3d3b4faa196e89fc30bb42b0
Xeift <xeift.c@gmail.com>
2024-03-25 14:19:29 +0800

    fix friend api

--- client/omelet/lib/api/post/remove_friend_api.dart
+++ client/omelet/lib/api/post/remove_friend_api.dart
@@ -5,10 +5,12 @@ import 'package:http/http.dart' as http;
 import 'package:omelet/utils/load_local_info.dart';
 
 Future<http.Response> removeFriendApi(String theirUid) async {
+  final token = await loadJwt();
   final res = await http.post(
     Uri.parse('$serverUri/api/v1/remove-friend'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $token'
     },
     body: jsonEncode(<String, String>{'theirUid': theirUid}),
   );
--- client/omelet/lib/api/post/reply_friend_request_api.dart
+++ client/omelet/lib/api/post/reply_friend_request_api.dart
@@ -6,10 +6,12 @@ import 'package:omelet/utils/load_local_info.dart';
 
 Future<http.Response> replyFriendRequestApi(
     String theirUid, bool isAgree) async {
+  final token = await loadJwt();
   final res = await http.post(
     Uri.parse('$serverUri/api/v1/reply-friend-request'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $token'
     },
     body: jsonEncode(
         <String, String>{'theirUid': theirUid, 'isAgree': isAgree.toString()}),

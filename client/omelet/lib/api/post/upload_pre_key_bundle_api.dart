493a9c58c2dd2cd7d8ce54392c6fe6a6dd950956
Xeift <xeift.c@gmail.com>
2024-03-25 14:21:55 +0800

    minor fixes

--- client/omelet/lib/api/post/upload_pre_key_bundle_api.dart
+++ client/omelet/lib/api/post/upload_pre_key_bundle_api.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'package:omelet/utils/load_local_info.dart';
 
 Future<http.Response> uploadPreKeyBundleApi(
     deviceId, ipkPub, spkPub, spkSig, opkPub) async {
-  const storage = FlutterSecureStorage();
-  final token = await storage.read(key: 'token');
+  final token = await loadJwt();
+
   final res =
       await http.post(Uri.parse('$serverUri/api/v1/upload-pre-key-bundle'),
           headers: <String, String>{

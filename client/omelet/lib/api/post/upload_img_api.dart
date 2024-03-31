ebbb257691a78a17119e7dd6bd7f7eeb3cc729e1
Xeift <xeift.c@gmail.com>
2024-03-31 12:56:40 +0800

    sync changes

--- client/omelet/lib/api/post/upload_img_api.dart
+++ client/omelet/lib/api/post/upload_img_api.dart
@@ -2,15 +2,43 @@ import 'package:http/http.dart' as http;
 
 import 'package:omelet/utils/load_local_info.dart';
 
-Future<http.StreamedResponse> uploadImgApi(
-    String encryptedBytes, String receiverUid) async {
+Future<http.StreamedResponse> uploadImgApi(deviceId, singleMsgInfo, receiverUid,
+    ourUid, theirUid, imgBytes, filename) async {
+  final (cihertext, isPreKeySignalMessage, spkId, opkId) = singleMsgInfo;
+
   final token = await loadJwt();
-  var request =
-      http.MultipartRequest('POST', Uri.parse('$serverUri/api/v1/upload-img'))
-        ..fields['receiverUid'] = receiverUid;
+  http.MultipartRequest request;
+
+  // 訊息格式為 PreKeySignalMessage
+  if (isPreKeySignalMessage) {
+    request =
+        http.MultipartRequest('POST', Uri.parse('$serverUri/api/v1/upload-img'))
+          ..fields['isPreKeySignalMessage'] = isPreKeySignalMessage.toString()
+          ..fields['type'] = 'image'
+          ..fields['sender'] = ourUid
+          ..fields['receiver'] = receiverUid
+          ..fields['receiverDeviceId'] = deviceId
+          ..fields['content'] = filename
+          ..fields['spkId'] = spkId.toString()
+          ..fields['opkId'] = opkId.toString();
+  }
+
+  // 訊息格式為 SignalMessage
+  else {
+    request =
+        http.MultipartRequest('POST', Uri.parse('$serverUri/api/v1/upload-img'))
+          ..fields['isPreKeySignalMessage'] = isPreKeySignalMessage.toString()
+          ..fields['type'] = 'image'
+          ..fields['sender'] = ourUid
+          ..fields['receiver'] = receiverUid
+          ..fields['receiverDeviceId'] = deviceId
+          ..fields['content'] = filename
+          ..fields['spkId'] = spkId.toString()
+          ..fields['opkId'] = opkId.toString();
+  }
   request.headers.addAll(<String, String>{'Authorization': 'Bearer $token'});
-  request.files.add(http.MultipartFile.fromString('imgData', encryptedBytes,
-      filename: 'img'));
+  request.files.add(
+      http.MultipartFile.fromString('imgData', cihertext, filename: 'img'));
   var response = await request.send();
 
   return response;

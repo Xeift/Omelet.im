0ed8dc362b493bb8d343b5efeeebb124ee0e3c22
Xeift <xeift.c@gmail.com>
2024-04-17 18:20:04 +0800

    fix notify backend type bug

--- backend/model/friendRequestModel.js
+++ backend/model/friendRequestModel.js
@@ -4,6 +4,7 @@ const FriendRequestSchema = new mongoose.Schema({
     initiatorUid: { type: String },
     targetUid: { type: String },
     timestamp: { type: Number },
+    type: { type: String },
 });
 const FriendRequestModel = mongoose.model('FriendRequest', FriendRequestSchema, 'friend_request');
 

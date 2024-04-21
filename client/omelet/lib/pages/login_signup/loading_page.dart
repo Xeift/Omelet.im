133e54de54bee381acb88ba899f92b5c709db02a
Xeift <xeift.c@gmail.com>
2024-04-21 18:19:45 +0800

    add request permission logic

--- client/omelet/lib/pages/login_signup/loading_page.dart
+++ client/omelet/lib/pages/login_signup/loading_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:omelet/api/debug_reset_prekeybundle_and_unread_msg.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
+import 'package:permission_handler/permission_handler.dart';
 
 import 'package:omelet/pages/message/chat_room_page.dart';
 import 'package:omelet/pages/nav_bar_control_page.dart';
@@ -50,6 +51,12 @@ class LoadingPageState extends State<LoadingPage> {
   Future<void> initSocket() async {
     print('[loading_page] 初始狀態檢查開始 --------------------');
     try {
+      PermissionStatus permission = await Permission.notification.status;
+      if (permission.isDenied) {
+        Permission.notification.request();
+      }
+      print(permission);
+
       // TODO: 刪除所有儲存空間、PreKeyBundle、UnreadMsg，debug 用
       // const storage = FlutterSecureStorage();
       // await storage.deleteAll();
@@ -100,7 +107,8 @@ class LoadingPageState extends State<LoadingPage> {
             String getTranslate =
                 await safeConfigStore.getTranslationDestLang(ourUid);
             if (getTranslate == 'null') {
-              await safeConfigStore.setTranslationDestLang(ourUid, 'Chinese Traditional');
+              await safeConfigStore.setTranslationDestLang(
+                  ourUid, 'Chinese Traditional');
             }
             getTranslate = await safeConfigStore.getTranslationDestLang(ourUid);
             print('[loading_page.dart]getTranslate:$getTranslate');
@@ -172,7 +180,8 @@ class LoadingPageState extends State<LoadingPage> {
         print(
             '[main.dart] currentActiveAccount 不存在❌\n該使用者第一次開啟 App，應跳轉至登入頁面並產生公鑰包\n');
         String ourUid = await loadCurrentActiveAccount();
-        await safeConfigStore.setTranslationDestLang(ourUid, 'Chinese Traditional');
+        await safeConfigStore.setTranslationDestLang(
+            ourUid, 'Chinese Traditional');
         if (mounted) {
           print('觸發跳轉');
           Navigator.of(context).push(MaterialPageRoute(

ec8d0e543e99311e6cff7cf51a3f59b5185a5e58
Xeift <xeift.c@gmail.com>
2024-04-21 14:30:23 +0800

    change app title

--- client/omelet/lib/main.dart
+++ client/omelet/lib/main.dart
@@ -29,7 +29,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Omelet Login Page',
+      title: 'Omelet.im',
       theme: Provider.of<ThemeProvier>(context).themeData,
       darkTheme: darkMode,
       initialRoute: '/',

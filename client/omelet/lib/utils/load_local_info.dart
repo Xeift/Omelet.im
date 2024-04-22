ae0a99a8ac4c778975d9739105dc24c96bc39e73
maplex18 <mapleiv2718@gmail.com>
2024-04-22 22:54:16 +0800

    profile pop

--- client/omelet/lib/utils/load_local_info.dart
+++ client/omelet/lib/utils/load_local_info.dart
@@ -4,9 +4,9 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'package:omelet/signal_protocol/safe_identity_store.dart';
 
-const serverUri = 'https://3d36-125-227-108-215.ngrok-free.app';
+// const serverUri = 'https://3d36-125-227-108-215.ngrok-free.app';
 // const serverUri = 'http://localhost:3000';
-// const serverUri = 'https://omelet.im:8443';
+const serverUri = 'https://omelet.im:8443';
 
 Future<void> changeCurrentActiveAccount(String newUid) async {
   const storage = FlutterSecureStorage();

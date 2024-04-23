b5d22f3c016bc7745d03769f2f1c3a0d42372dcb
Xeift <xeift.c@gmail.com>
2024-04-23 15:27:04 +0800

    rename func

--- client/omelet/lib/storage/safe_device_id_store.dart
+++ client/omelet/lib/storage/safe_device_id_store.dart
@@ -7,25 +7,34 @@ import 'package:omelet/utils/load_local_info.dart';
 class SafeDeviceIdStore {
   final storage = const FlutterSecureStorage();
 
-  Future<void> updateOthersDeviceIds(
+  Future<void> updateTheirDeviceIds(
       String theirUid, List<String> deviceIdList) async {
     final ourUid = await loadCurrentActiveAccount();
     await storage.write(
         key: '${ourUid}_deviceId_$theirUid', value: jsonEncode(deviceIdList));
   }
 
-  Future<void> removeOthersDeviceIds(String theirUid) async {
+  Future<void> removeTheirDeviceIds(String theirUid) async {
     final ourUid = await loadCurrentActiveAccount();
     await storage.delete(key: '${ourUid}_deviceId_$theirUid');
   }
 
-  Future<List<String>> getOthersDeviceIds(String theirUid) async {
+  Future<List<String>> getOurDeviceIds(String theirUid) async {
     final ourUid = await loadCurrentActiveAccount();
-    final othersDeviceIds =
-        await storage.read(key: '${ourUid}_deviceId_$theirUid');
-    if (othersDeviceIds == null) {
+    final ourDeviceIds = await storage.read(key: '${ourUid}_deviceId_$ourUid');
+    if (ourDeviceIds == null) {
       return [];
     }
-    return List<String>.from(jsonDecode(othersDeviceIds));
+    return List<String>.from(jsonDecode(ourDeviceIds));
+  }
+
+  Future<List<String>> getTheirDeviceIds(String theirUid) async {
+    final ourUid = await loadCurrentActiveAccount();
+    final theirDeviceIds =
+        await storage.read(key: '${ourUid}_deviceId_$theirUid');
+    if (theirDeviceIds == null) {
+      return [];
+    }
+    return List<String>.from(jsonDecode(theirDeviceIds));
   }
 }

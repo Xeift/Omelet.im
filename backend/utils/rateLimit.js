f0fa0d6e377887b8beb40afd35d06404c2d81d37
Xeift <xeift.c@gmail.com>
2024-05-10 11:07:40 +0800

    edit ratelimit time

--- backend/utils/rateLimit.js
+++ backend/utils/rateLimit.js
@@ -1,6 +1,6 @@
 const rateLimit = require('express-rate-limit');
 const authLimiter = rateLimit({
-    windowMs: 5 * 60 * 1000,
+    windowMs: 1 * 60 * 1000,
     max: 100,
     handler: function(req, res) {
         res.status(429).json({
